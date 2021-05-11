ac4d58760742f6b1cc5ea0597e5466f03980c2f3
Edward Chung <munjo5746@gmail.com>
2021-05-10 21:09:26 -0400

    glass types select added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import './App.scss';
-import { PageHeader } from 'antd';
+import { Input, PageHeader, Select } from 'antd';
+import { Form } from 'antd';
 
 function App() {
+    const [form] = Form.useForm();
+    const glassTypes = React.useMemo(
+        () => [
+            { label: 'CLEAR TYPE', value: 'clear_type' },
+            { label: 'BRONZE/GRAY', value: 'bronze_gray' },
+            { label: 'OPTIWHITE', value: 'optiwhite' },
+            { label: 'SINGLE SIDE ACID', value: 'single_side_acid' },
+            { label: 'ACID/STARPHIRE', value: 'acid_starphire' },
+            { label: 'LOW-E', value: 'low_e' },
+            { label: 'MIRROR', value: 'mirror' },
+            { label: 'STARPHIRE MIRROR', value: 'starphire_mirror' },
+            { label: 'MIRRORPANE', value: 'mirrorpane' },
+        ],
+        [],
+    );
     return (
         <PageHeader title="Title" subTitle="Calculator">
             <div className="app">
-                <div className="form">aaa</div>
+                <div className="form">
+                    <Form form={form} layout="vertical">
+                        <Form.Item label="Glass Type">
+                            <Select>
+                                {glassTypes.map((glassType) => (
+                                    <Select.Option
+                                        key={`glass-type-option-${glassType.value}`}
+                                        value={glassType.value}
+                                    >
+                                        {glassType.label}
+                                    </Select.Option>
+                                ))}
+                            </Select>
+                        </Form.Item>
+                        <Form.Item label="Dimension" name="dimension">
+                            <div className="dimension">
+                                <Input placeholder="width" />
+                                <div>x</div>
+                                <Input placeholder="height" />
+                            </div>
+                        </Form.Item>
+                    </Form>
+                </div>
                 <div className="summary">aaa</div>
             </div>
         </PageHeader>
