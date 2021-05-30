d7eae0e7282540ea977a99a931d5868e3148c775
Edward Chung <munjo5746@gmail.com>
2021-05-30 10:16:07 -0400

    Menu scrollable and flex display

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,17 @@ import styles from './App.module.scss';
 const App: React.FC = () => {
     return (
         <div className={styles.App}>
-            <div className={styles.Header}></div>
+            <div className={styles.Header}>
+                <div className={styles.Title}>Cafe Terrace</div>
+            </div>
             <div className={styles.MenuItemWrapper}></div>
-            <div className={styles.Menu}></div>
+            <div className={styles.Menu}>
+                {['Coffee', 'Tea', 'Tart', 'Cake'].map((menuType) => {
+                    return (
+                        <span key={`menu-type-${menuType}`}>{menuType}</span>
+                    );
+                })}
+            </div>
         </div>
     );
 };
