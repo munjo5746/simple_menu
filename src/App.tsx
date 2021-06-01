0a09dab34e5a77d8685b343403c11ee1ff2adc5a
Edward Chung <munjo5746@gmail.com>
2021-06-01 13:24:57 -0400

    basic price layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from './App.module.scss';
 
 const App: React.FC = () => {
+    const [itemsUnderSelectedMenu, setItemsUnderSelectedMenu] = React.useState<
+        string[]
+    >(['item1', 'item2']);
     return (
         <div className={styles.App}>
             <div className={styles.Header}>
@@ -12,13 +15,30 @@ const App: React.FC = () => {
                 </div>
             </div>
             <div className={styles.Menu}>
-                {['Coffee', 'Tea', 'Tart', 'Cake'].map((menuType) => {
+                {['Coffee', 'Tea', 'Tart', 'Cake', 'Misc'].map((menuType) => {
                     return (
                         <span key={`menu-type-${menuType}`}>{menuType}</span>
                     );
                 })}
             </div>
-            <div className={styles.MenuItemWrapper}></div>
+            <div className={styles.MenuItemWrapper}>
+                {itemsUnderSelectedMenu.map((item) => {
+                    return (
+                        <div
+                            key={`item-${item}`}
+                            className={`${styles.Card} ${styles.Item}`}
+                        >
+                            <div className={styles.Name}>Iced Coffee</div>
+                            <div className={styles.PriceList}>
+                                <span className={styles.Price}>
+                                    <span className={styles.Label}>sm:</span>
+                                    <span className={styles.Value}>$3.99</span>
+                                </span>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
